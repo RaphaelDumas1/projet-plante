89fd71badda0f9e5a24be624f34ecd08aa3077ad
Jeannegbl <jeanne.gablin@gmail.com>
2022-11-25 12:47:33 +0100

    test

---
+++ src/Controller/HomeControler.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeControler extends AbstractController
+{
+    /**
+    * @Route("/Home", name="Home")
+    */
+    public function home(): Response
+    {
+        return $this->render('Plantes/home.html.twig');
+    }
+}
