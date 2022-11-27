21cb8b1c2071d4d0e5abcb7ed49945cbd31736f0
Jeannegbl <jeanne.gablin@gmail.com>
2022-11-27 16:47:37 +0100

    debut partie admin : voir plantes et modifier plantes

--- src/Controller/HomeControler.php
+++ src/Controller/HomeControler.php
@@ -5,6 +5,16 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+use App\Repository\PlanteRepository;
+use App\Repository\TexteBeforeRepository;
+use App\Repository\TexteAfterRepository;
+
+use App\Entity\Plante;
+
+use App\Form\PlanteType;
+
 
 class HomeControler extends AbstractController
 {
@@ -38,5 +48,56 @@ class HomeControler extends AbstractController
     public function home(): Response
     {
         return $this->render('Plantes/home.html.twig');
+    }
+
+    /**
+    * @Route("/admin", name="Admin")
+    */
+    public function admin(): Response
+    {
+        return $this->render('Admin/admin.html.twig');
+    }
+
+    /**
+    * @Route("/admin/plantes", name="Admin-plantes")
+    */
+    public function admin_plantes(PlanteRepository $repository): Response
+    {
+        $plantes = $repository->findAll();
+        return $this->render('Admin/plantes.html.twig', ['plantes' => $plantes,]);
+    }
+
+    /**
+    * @Route("/admin/plantes/{id}", name="Admin-plante-info")
+    */
+    
+    public function admin_plantes_info(PlanteRepository $repository, int $id,TexteBeforeRepository $repository2, TexteAfterRepository $repository3): Response
+    {
+        $plantes = $repository->findBy(array('id' => $id));
+        $text_before = $repository2->findBy(array('id' => $id));
+        $text_after = $repository3->findBy(array('id' => $id));
+        return $this->render('Admin/infoplantes.html.twig', [
+            'plantes' => $plantes, 'text_before' => $text_before, 'text_after' => $text_after
+        ]);
+    }
+
+    /**
+    * @Route("/admin/plantes/modifier/{id}", name="Modifier-plante")
+    */
+    
+    public function plante_modif(Plante $plante, PlanteRepository $repository, Request $request)
+    {
+        $form = $this->createForm(PlanteType::class, $plante);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $repository->save($plante, true);
+
+            return $this->redirectToRoute('Admin-plante-info', ['id' => $plante->getId()]);
+         }
+
+        return $this->render('Admin/modifplante.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
