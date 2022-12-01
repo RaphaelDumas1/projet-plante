9755b656ee54cda95b28c3d0b0292e283e64a805
Jeannegbl <jeanne.gablin@gmail.com>
2022-12-01 15:57:42 +0100

    connexion - deconnexion - inscription

--- src/Controller/HomeControler.php
+++ src/Controller/HomeControler.php
@@ -31,22 +31,6 @@ class HomeControler extends AbstractController
     }
 
     /**
-    * @Route("/inscription", name="Inscription")
-    */
-    public function inscription(): Response
-    {
-        return $this->render('Plantes/inscription.html.twig');
-    }
-
-    /**
-    * @Route("/connexion", name="Connexion")
-    */
-    public function connexion(): Response
-    {
-        return $this->render('Plantes/connexion.html.twig');
-    }
-
-    /**
     * @Route("/home", name="Menu")
     */
     public function home(): Response
