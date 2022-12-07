dfe659a0969b6ff1410990a37151cd690edd1353
Jeannegbl <jeanne.gablin@gmail.com>
2022-12-07 12:59:05 +0100

    modification de mot de passe + effacer compte

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,10 +6,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 use App\Form\UserType;
+use App\Form\UserPasswordType;
 
 
 class UserController extends AbstractController
@@ -47,5 +50,54 @@ class UserController extends AbstractController
         return $this->render('Utilisateur/comptemodifier.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+    * @Route("/Compte/Modifier/mdp", name="compte-modifier-mdp")
+    */
+    public function compte_modifier_mdp(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $manager)
+    {
+        if (!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        }
+        $user = $this->getUser();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+            $manager->persist($user);
+            $manager->flush();
+
+            return $this->redirectToRoute('compte');
+        }
+        return $this->render('Utilisateur/comptemodifiermdp.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+    * @Route("/Compte/Effacer", name="compte-effacer")
+    */
+    public function compte_effacer(Request $request, EntityManagerInterface $manager, Session $session)
+    {
+        if (!$this->getUser()){
+            return $this->redirectToRoute('app_login');
+        }
+        $user = $this->getUser();
+
+        $manager->remove($user);
+        $manager->flush();
+
+        $session = new Session();
+        $session->invalidate();
+  
+        return $this->redirectToRoute('Premiere_page');
     }
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -7,13 +7,15 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('email')
-            ->add('password')
+            ->add('password', PasswordType::class)
             ->add('pseudo')
             ->add('photo')
         ;
