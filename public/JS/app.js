cf0f35c67359e5201534ebd5e890a8fed79fe810
RaphaelDumas1 <96191736+RaphaelDumas1@users.noreply.github.com>
2022-12-11 00:43:57 +0100

    style jeu degeu

--- public/JS/app.js
+++ public/JS/app.js
@@ -4,6 +4,7 @@ let boutton_photo = document.querySelector("#boutton-photo");
 let photo = document.querySelector("#photo");
 let nom_plante = document.querySelector("#nom_plante").textContent;
 let ajax_path = $("#ajax-path").attr("ajax-path");
+let boutton_upload = document.querySelector("#boutton-upload");
 var latitude, longitude, date_photo, date_valide;
 
 boutton_camera.addEventListener('click', async function () {
@@ -23,15 +24,20 @@ boutton_photo.addEventListener('click', function () {
     let jour = date.getDate();
     let mois = date.getMonth() + 1;
     let annee = date.getFullYear();
-    date_photo = `${jour}-${mois}-${annee}`
+    date_photo = `${jour}-${mois}-${annee}`;
+    boutton_upload.style.display = "unset";
     photo.getContext('2d').drawImage(camera, 0, 0, photo.width, photo.height);
 });
 
 // a retravailler 
 function trouver() {
+    let div2 = document.querySelector("#div2");
+    div2.style.display = "none";
     const newDiv = document.createElement("div");
     const newContent = document.createTextNode(`Felicitation tu a ajouter le ${nom_plante} a ta collection`);
     newDiv.appendChild(newContent);
+    newDiv.classList.add("texte");
+    newDiv.classList.add("en-tete");
     const currentDiv = document.getElementById("div1");
     document.body.insertBefore(newDiv, currentDiv);
     setTimeout(location.reload(true), 6000);
